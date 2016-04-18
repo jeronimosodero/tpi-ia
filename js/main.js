f36c390b2628b8767db9ed6d716d15b75daa40be
Nelson Yaccuzzi <Nelson Yaccuzzi>
2016-04-18 20:15:09 -0300

    error y generacion de luciernagas

--- js/main.js
+++ js/main.js
@@ -14,10 +14,15 @@ luc1["O"] = 6;
 luc1["Y"] = 7;
 //console.log(luc1["S"]);
 */
-var op1 = "SEND";
-var op2 = "MORE";
-var op3 = "MONEY";
-var suma =op1  + op2 + op3;
+
+var operador1 = "SEND";
+var operador2 = "MORE";
+var resultado = "MONEY";
+
+var op1 = operador1.split("").reverse();
+var op2 = operador2.split("").reverse();
+var res = resultado.split("").reverse();
+var suma =operador1  + operador2 + resultado;
 var unicos = [];
 for (i = 0;i < suma.length; i++) {
 	if(unicos.indexOf(suma[i])==-1){
@@ -26,8 +31,21 @@ for (i = 0;i < suma.length; i++) {
 }
 
 var luciernagas = generarLuciernagas(10,unicos);
-console.log(luciernagas);
+	
+	for(x in luciernagas){
+		console.log(luciernagas[x])
+		console.log(error(op1,op2,res,luciernagas[x]))
+	}
 
+var luc1 = {};
+luc1["S"] = 9;
+luc1["E"] = 5;
+luc1["N"] = 6;
+luc1["D"] = 7;
+luc1["R"] = 8;
+luc1["M"] = 1;
+luc1["O"] = 0;
+luc1["Y"] = 2;
 
 function generarLuciernagas(cant,unicos){
 	var luciernagas = [];
@@ -53,4 +71,27 @@ function exists(luciernaga, nro) {
     	}
     }
     return false;
+}
+
+function error(op1,op2,res,luc){
+
+	var acarreo=0;
+	var h=0;
+	var error=0;
+
+	for (var i = 0; i < res.length; i++) {
+		h= acarreo - luc[res[i]];
+		acarreo=0;
+
+		if(i<op1.length) h=h+luc[op1[i]]
+		if(i<op2.length) h=h+luc[op2[i]]
+
+
+		if(h>=10){
+			h=h-10;
+			acarreo=1;
+		}
+		error+=Math.abs(h);
+	};
+	return error;
 }
